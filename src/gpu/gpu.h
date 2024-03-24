aa279384df2b4d17bf01aa0bf949fd78fc45028f
Janet Blackquill <uhhadd@gmail.com>
2024-03-24 14:39:57 -0400

    make GPU use libepoxy
    glew is old and has many issues that prevent it from running on modern systems
    also port to meson because i'm not touching makefile hell

--- src/gpu/gpu.h
+++ src/gpu/gpu.h
@@ -3,15 +3,10 @@
 
 #include "../orbit.h"
 
-#ifdef _WIN32
-#define SDL_MAIN_HANDLED
-#endif
-
+#include <epoxy/gl.h>
 #include <SDL2/SDL.h>
-#include "../GL/glew.h"
 #include <SDL2/SDL_opengl.h>
 #include <SDL2/SDL_image.h>
-#include <GL/gl.h>
 
 extern bool gpu_is_drawing;
 extern u64  gpu_framebuf;
